e9f308a3d6e4dca7b149028548e999fc230dc4c9
Bruno Arenzana <brunoarenzana@gmail.com>
2026-04-22 21:25:48 -0300

    anda a todos menos marce

--- src/admin/dto/update-admin.input.ts
+++ src/admin/dto/update-admin.input.ts
@@ -1,8 +1,6 @@
 import { CreateAdminInput } from './create-admin.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
-@InputType()
-export class UpdateAdminInput extends PartialType(CreateAdminInput) {
-  @Field(() => Int)
+
+export declare class UpdateAdminInput extends CreateAdminInput {
   id: number;
 }
