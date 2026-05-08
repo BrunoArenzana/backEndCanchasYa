36bccdbdd8684c4fb7941aed3311065258839025
nahuefalconaro <nfalconaro5@gmail.com>
2026-05-08 19:30:42 -0300

    add: cron

--- src/cron/reviewPay.cron.ts
+++ src/cron/reviewPay.cron.ts
@@ -1,5 +1,7 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { Cron, CronExpression } from "@nestjs/schedule";
+import { Cancha } from "src/cancha/entities/cancha.entity";
+import { Club } from "src/club/entities/club.entity";
 
 @Injectable()
 export class ReviewPayCron {
@@ -9,6 +11,10 @@ export class ReviewPayCron {
     @Cron(CronExpression.EVERY_5_SECONDS)
     handleCron() {
         this.logger.debug('Revisando pagos pendientes...');
+        const c = new Club();// traer de la base de datos
+        // recorrer todos los clubes
+        if (c.created_at < new Date()) {
+            c.estado = 'inactivo';
+        }
     }
-
 }
