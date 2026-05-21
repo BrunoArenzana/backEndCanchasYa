464e3040b5639f3399185c0b5be89c4eccfec644
Bruno Arenzana <brunoarenzana@gmail.com>
2026-05-21 20:37:24 -0300

    admin, club y usuarios LOGGIN

--- src/admin/dto/create-admin.dto.ts
+++
@@ -1,5 +0,0 @@
-export class CreateAdminDto {
-  nombre_admin!: string;
-  email_admin!: string;
-  password_hash!: string;
-}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,5 +1,4 @@
     import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, OneToMany, ManyToMany, JoinColumn, JoinTable } from 'typeorm';
-import { Admin } from '../../admin/entities/admin.entity';
 import { Club } from '../../club/entities/club.entity';
 import { Cancha } from '../../cancha/entities/cancha.entity';
 import { Reserva } from '../../reserva/entities/reserva.entity';
@@ -45,9 +44,9 @@ export class User {
     })
     tipo_usuario!: string;
     
-    @ManyToOne(() => Admin, { nullable: true, onDelete: 'SET NULL' })
+    @ManyToOne(() => User, { nullable: true, onDelete: 'SET NULL' })
     @JoinColumn({ name: 'id_admin_aprobado' })
-    admin_aprobado!: Admin;
+    admin_aprobado!: User | null;
     @CreateDateColumn({ name: 'created_at', type: 'datetime' })
     created_at!: Date;
 
