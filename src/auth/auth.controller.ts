42692aaecef97d1f8b01a0237818da780ebf5586
Bruno Arenzana <38697614+BrunoArenzana@users.noreply.github.com>
2026-06-25 18:54:36 -0300

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -22,12 +22,12 @@ export class AuthController {
     return this.authService.registerDueno(body, file);
   }
 
-  @Post('forgot-password/send-code')
+  @Post('id: number-password/send-code')
   sendPasswordResetCode(@Body() body: any) {
     return this.authService.sendPasswordResetCode(body.email);
   }
 
-  @Post('forgot-password/reset')
+  @Post('id: number-password/reset')
   resetPassword(@Body() body: any) {
     return this.authService.resetPassword(
       body.email,
